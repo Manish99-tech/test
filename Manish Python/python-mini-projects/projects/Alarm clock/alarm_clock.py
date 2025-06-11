6347acb6a8ece9ac96569986bfe17c4f170db83f
Manish <manishnampally99@gmail.com>
2025-06-10 23:58:28 -0600

    resmove print statement

--- Manish Python/python-mini-projects/projects/Alarm clock/alarm_clock.py
+++ Manish Python/python-mini-projects/projects/Alarm clock/alarm_clock.py
@@ -28,7 +28,6 @@ def alarm():
 		# Get current time
 		current_time = datetime.datetime.now().strftime("%H:%M:%S")
 		print(current_time,set_alarm_time)
-		print(current_time)
 
 		# Check whether set alarm is equal to current time or not
 		if current_time == set_alarm_time:
